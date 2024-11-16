5ce9d0811b5b7ca32fcf7f530e569aaca07d9fb3
Jian Sun <sunjxan@gmail.com>
2024-11-17 00:59:32 +0800

    Update matmul.cpp

--- src/matmul.cpp
+++ src/matmul.cpp
@@ -2,7 +2,19 @@
 #include <torch/extension.h>
 #include "matmul.h"
 
-torch::Tensor forward(const torch::Tensor a, const torch::Tensor b) {
+#define CHECK_CUDA(x) \
+  TORCH_CHECK(x.is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CONTIGUOUS(x) \
+  TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+#define CHECK_INPUT(x) \
+  CHECK_CUDA(x);       \
+  CHECK_CONTIGUOUS(x)
+
+torch::Tensor matmul(const torch::Tensor a, const torch::Tensor b)
+{
+    CHECK_INPUT(a);
+    CHECK_INPUT(b);
+
     unsigned m = a.size(0), n = b.size(1), k = b.size(0);
     torch::Tensor c = torch::zeros({m, n}, torch::dtype(torch::kFloat32).device(torch::kCUDA));
     float *p_c = c.data_ptr<float>();
@@ -12,44 +24,12 @@ torch::Tensor forward(const torch::Tensor a, const torch::Tensor b) {
     return c;
 }
 
-torch::Tensor backward_left(const torch::Tensor a, const torch::Tensor b) {
-    unsigned m = a.size(0), n = b.size(1), k = b.size(0);
-    torch::Tensor c = torch::zeros({m, k}, torch::dtype(torch::kFloat32).device(torch::kCUDA));
-    for (unsigned i = 0; i < k; ++i) {
-        float sum = 0;
-        for (unsigned j = 0; j < n; ++j) {
-            sum += b[i][j].item<float>();
-        }
-        for (unsigned j = 0; j < m; ++j) {
-            c[j][i] = sum;
-        }
-    }
-    return c;
+PYBIND11_MODULE(torch_ops_matmul, m)
+{
+    m.def("forward", matmul);
 }
 
-torch::Tensor backward_right(const torch::Tensor a, const torch::Tensor b) {
-    unsigned m = a.size(0), n = b.size(1), k = b.size(0);
-    torch::Tensor c = torch::zeros({k, n}, torch::dtype(torch::kFloat32).device(torch::kCUDA));
-    for (unsigned i = 0; i < k; ++i) {
-        float sum = 0;
-        for (unsigned j = 0; j < m; ++j) {
-            sum += a[j][i].item<float>();
-        }
-        for (unsigned j = 0; j < n; ++j) {
-            c[i][j] = sum;
-        }
-    }
-    return c;
-}
-
-PYBIND11_MODULE(torch_ops_matmul, m) {
-    m.def("forward", forward);
-    m.def("backward_left", backward_left);
-    m.def("backward_right", backward_right);
-}
-
-TORCH_LIBRARY(torch_ops_matmul, m) {
-    m.def("forward", forward);
-    m.def("backward_left", backward_left);
-    m.def("backward_right", backward_right);
+TORCH_LIBRARY(torch_ops_matmul, m)
+{
+    m.def("forward", matmul);
 }
