bf779a92c8471028ce1b301f03b1a1fb54b84e69
Jian Sun <sunjxan@gmail.com>
2024-11-18 01:21:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch, op
-a1 = torch.randn((2, 3), requires_grad=True, device="cuda:0")
-b1 = torch.randn((3, 4), requires_grad=True, device="cuda:0")
+a1 = torch.randn((2, 3), dtype=torch.float32, requires_grad=True, device="cuda:0")
+b1 = torch.randn((3, 4), dtype=torch.float32, requires_grad=True, device="cuda:0")
 c1 = torch.matmul(a1, b1)
 d1 = c1 * 5
 d1.sum().backward()
@@ -9,6 +9,6 @@ b2 = b1.clone().detach().requires_grad_(True)
 c2 = op.matmul_op(a2, b2)
 d2 = c2 * 5
 d2.sum().backward()
-print(torch.allclose(c1, c2, atol=1e-5))
-print(torch.allclose(a1.grad, a2.grad, atol=1e-5))
-print(torch.allclose(b1.grad, b2.grad, atol=1e-5))
+print(torch.allclose(c1, c2, atol=1e-2))
+print(torch.allclose(a1.grad, a2.grad, atol=1e-2))
+print(torch.allclose(b1.grad, b2.grad, atol=1e-2))
