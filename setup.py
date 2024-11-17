747a047a333c20de5a171f5eb739650057ac88e8
Jian Sun <sunjxan@gmail.com>
2024-11-17 17:38:12 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+ext_modules = [
+    CUDAExtension(
+        "torch_ops_matmul", [
+            "src/matmul_kernel.cu",
+            "src/matmul.cpp"
+        ],
+        include_dirs=[
+            "include"
+        ]
+    ) 
+]
+setup(
+    name="torch_ops_matmul",
+    version="1.0",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
+    zip_safe=False
+)
