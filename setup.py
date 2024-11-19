d447044d1a47d24be82b5bbd1d46e9a08d868c00
Jian Sun <sunjxan@gmail.com>
2024-11-20 00:33:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 ext_modules = [
     CUDAExtension(
-        "torch_ops", [
+        "torch_ops_matmul", [
             "src/cuda/matmul.cu"
         ],
         include_dirs=[
@@ -12,7 +12,8 @@ ext_modules = [
 setup(
     name="torch_ops",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"":"src"},
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
     zip_safe=False
